8180ce29e90c4369859f1003cd6957a706c35114
Clark Joy Pesco <clarkjoypesco@yahoo.com>
2019-04-09 07:33:43 +0800

    initial commit

---
+++ bodacious.py
@@ -0,0 +1,9 @@
+# Given the variables z and s defined as:
+z = 'clark'
+s = 'bodacious'
+# write Python code that prints out clacious
+# without using any quote characters in
+# your code.
+
+print z[0:2] + s[3:]
+
